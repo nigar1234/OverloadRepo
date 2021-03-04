e3360aced0023fd30932bfe45b574fed4545a324
qatester <qatester@192.168.0.14>
2021-03-03 20:18:31 -0500

    This is my second Project

---
+++ src/method_Overload/Overload_Method.java
@@ -0,0 +1,14 @@
+package method_Overload;
+
+public class Overload_Method {
+
+	public static void main(String[] args) {
+		 OverLoad1 ob = new OverLoad1();
+		 
+		 ob.add(3, 4, 5);
+		 ob.add(2.0, 3.0);
+		 ob.add(4, 5);
+         ob.add();
+	}
+
+}
